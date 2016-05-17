01cf2b122aee13f51ac9f0d2085d539b13477266
Alex Dao <rahx1t@gmail.com>
2016-05-17 16:15:28 -0400

    remove services directory

--- www/js/factories/socket.js
+++ www/js/factories/socket.js
@@ -1,16 +1,4 @@
-app.factory('socket', function (socketFactory) {
-  //create socket and connect to http://chat.socket.io
-  var myIOSocket = io.connect('http://chat.socket.io');
-  // var myIOSocket = io.connect('http://localhost:3000');
-  mySocket = socketFactory({
-    ioSocket: myIOSocket
-  });
-
-  return mySocket;
-
-})
-
-app.factory('localSocket', function (socketFactory) {
+app.factory('ServerSocketFactory', function (socketFactory) {
   //create socket and connect to http://chat.socket.io
   // var myIOSocket = io.connect('http://chat.socket.io');
   var myIOSocket = io.connect('http://localhost:3000');
