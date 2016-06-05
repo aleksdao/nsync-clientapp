f1ded6e1d1d3aaf1b424614d99896430fdc9fa46
John Hill <john@proofpositiveimaging.com>
2016-06-05 16:33:10 -0400

    cuts and fades work

--- www/js/controllers/show.Controller.js
+++ www/js/controllers/show.Controller.js
@@ -5,15 +5,18 @@ app.controller('ShowController', function ($scope, $state, socket, SequenceHandl
 
 
 
-  SequenceHandler.init({body: '#showText'});
+  SequenceHandler.init({
+      container: '#showPage',
+      title:  '#showTitle',
+      body: '#showText'
+      });
 
   $scope.itemColor = 'red';
   socket.startPingRepeat(); //poll server until we start show
 
-  //var poo = angular.element(document.querySelector('#showText'));
   socket.on('play',function(data){
     SequenceHandler.loadSequence(data.sequence);
-    console.log(socket.getLatency())
+    console.log(socket.getLatency());
     if(SequenceHandler.getTransportState() === 'stopped'){
       //play the sequence
       SequenceHandler.queueStart(data.startTime, true);
